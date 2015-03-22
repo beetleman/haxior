4a2ef44d6fc53aac220b5e2c3389ca12557df3a3
beetleman <mateusz.probachta@gmail.com>
2015-03-22 10:18:58 +0100

    Improve url replacment.

--- proxy/views.py
+++ proxy/views.py
@@ -6,41 +6,51 @@ import httplib2
 from urllib import urlencode
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-
+import re
 
 from models import Dane
 
 PROXY_FORMAT = u"http://%s/%s" % (settings.PROXY_DOMAIN, u"%s")
+RE_HAX = re.compile(
+    '(%s.+index.php)|(%s.+view.php)' % (
+        settings.PROXY_DOMAIN,
+        settings.PROXY_DOMAIN
+    ),
+    re.VERBOSE
+)
+
+
+def replacer(match):
+    text = match.group()
+    # import ipdb; ipdb.set_trace()
+    return text.replace(
+        settings.PROXY_DOMAIN,
+        settings.HAXIOR_DOMAIN
+    )
 
 
 @csrf_exempt
 def proxy(request, url):
     conn = httplib2.Http()
-
+    m = Dane()
     if request.method == "GET":
         url_ending = "%s?%s" % (url, urlencode(request.GET))
         url = PROXY_FORMAT % url_ending
         resp, content = conn.request(url, request.method)
+        m.type = m.GET
         data_to_save = dict(request.GET.iterlists())
-        if data_to_save:
-            m = Dane()
-            m.type = m.GET
-            m.data = str(data_to_save)
-            m.url = url
-            m.save()
-        return HttpResponse(content.replace(
-            '%s/login/index.php' % settings.PROXY_DOMAIN,
-            '%s/login/index.php' % settings.HAXIOR_DOMAIN
-        ))
     elif request.method == "POST":
         url = PROXY_FORMAT % url
         data = urlencode(request.POST)
         resp, content = conn.request(url, request.method, data)
+        m.type = m.POST
         data_to_save = dict(request.POST.iterlists())
-        if data_to_save:
-            m = Dane()
-            m.type = m.POST
-            m.data = str(data_to_save)
-            m.url = url
-            m.save()
-        return HttpResponse(content)
+    if data_to_save:
+        m = Dane()
+        m.data = str(data_to_save)
+        m.url = url
+        m.save()
+    return HttpResponse(RE_HAX.sub(
+        replacer,
+        content
+    ))
