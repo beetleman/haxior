d82be0076fe29ad89f8191c8826427c9a8ee2e7b
beetleman <mateusz.probachta@gmail.com>
2015-03-22 06:18:07 +0100

    add `Data` model to admin

--- proxy/admin.py
+++ proxy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Dane
 
 # Register your models here.
+
+
+class DaneAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Dane, AuthorAdmin)
