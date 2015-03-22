66cf7aa63e57ef1025c081c00f3dde75df2bb069
beetleman <mateusz.probachta@gmail.com>
2015-03-22 07:19:19 +0100

    change admin url

--- haxior/urls.py
+++ haxior/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
                        # url(r'^$', 'haxior.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
 
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^haxior_admin/', include(admin.site.urls)),
                        url(r'^(?P<url>.*)$', proxy),
                        )
